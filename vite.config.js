82a7d58fe3507d6f36dbf98752cd810cb7ce6468
brian <dev.brianoctavio@gmail.com>
2026-05-17 17:25:31 -0600

    build: configure automated deployment with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/dev-brian.github.io/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
